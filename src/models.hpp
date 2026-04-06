f4cdef2e2c9eecfa48d8759046b7741120b9ad03
s1nhronn <silekatik@yandex.ru>
2026-04-06 20:27:54 +0300

    Some interface

---
+++ src/models.hpp
@@ -0,0 +1,25 @@
+#ifndef MODELS_HPP
+#define MODELS_HPP
+#include <string>
+#include <unordered_map>
+
+namespace models
+{
+  struct Request
+  {
+    std::string method;
+    std::string host;
+    std::string path;
+    std::unordered_map< std::string, std::string > headers;
+    std::string body;
+  };
+
+  struct Response
+  {
+    std::string status_line;
+    std::unordered_map< std::string, std::string > headers;
+    std::string body;
+    std::string error;
+  };
+}
+#endif
