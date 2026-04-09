e3d45f04c35a39a32b4b4c70d0a0f60fe97687ec
s1nhronn <silekatik@yandex.ru>
2026-04-09 21:42:08 +0300

    Changes in struct of project + test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,16 @@
+#include <memory>
+#include <cli/cli.h>
+#include <cli/clilocalsession.h>
+#include <cli/loopscheduler.h>
+#include "commands/commands.hpp"
+
 int main()
-{}
+{
+  std::unique_ptr< cli::Menu > rootMenu = commands::init();
+
+  cli::Cli app(std::move(rootMenu));
+  cli::LoopScheduler scheduler;
+  cli::CliLocalTerminalSession session(app, scheduler, std::cout);
+
+  scheduler.Run();
+}
