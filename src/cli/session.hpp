1d09dbac09d50af1376b814a12b1fbb78fc3f101
s1nhronn <silekatik@yandex.ru>
2026-04-09 10:45:45 +0300

    Interface of session

---
+++ src/cli/session.hpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include "../../libs/json.hh"
+#include "../common/models.hpp"
+
+using json = nlohmann::json;
+
+namespace session
+{
+  class Session
+  {
+  public:
+    Session();
+    Session(const Session& rhs);
+    Session(Session&& rhs);
+    explicit Session(std::string name);
+    ~Session();
+    Session operator=(const Session& rhs);
+    Session operator=(Session&& rhs);
+
+    std::string getName() const noexcept;
+    void setName();
+    Session removeSession();
+    Session switchSession(const std::string& name);
+    const json& getHistory(const std::string& name, size_t limit = 10) const noexcept;
+    void save();
+    void setMark(const std::string& markName);
+    void setMark(const std::string& reqName, const std::string& markName);
+    void setComment(const std::string& comment);
+    void setComment(const std::string& reqName, const std::string& comment);
+    void addRequest(const std::string& name, const models::Request& request);
+    void changeRequest(const std::string& name, const models::Request& request);
+
+  private:
+    std::string name_ = "Unknown";
+    json history;
+  };
+
+  std::vector< std::string > sessionList();
+}
