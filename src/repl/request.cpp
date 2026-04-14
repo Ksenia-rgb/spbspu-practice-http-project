78bf7b4dc5f8992a87e4db316058a340815e312b
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-04-14 15:24:26 +0300

    split req() by functions

--- src/repl/request.cpp
+++ src/repl/request.cpp
@@ -1,78 +1,121 @@
 #include "request.hpp"
 #include <iostream>
+#include "../../libs/json.hh"
+
+using json = nlohmann::json;
 
 namespace http
 {
   namespace repl
   {
-    void req(std::ostream& out)
+    namespace req
     {
-      std::istream& in = std::cin;
-      models::Request request;
-      std::string name;
-      out << "req name> ";
-      std::getline(in, name);
-      out << "req method> ";
-      std::getline(in, request.method);
-      std::string url;
-      out << "req URL> ";
-      std::getline(in, url);
-      size_t i = url.find("://") + 3;
-      if (i == std::string::npos)
+      void setName(std::ostream& out, std::string& name, const std::string& new_name)
       {
-        i = 0;
+        name = new_name;
       }
-      for (; url[i] != '/'; ++i)
-      {
-        request.host += url[i];
-      }
-      for (; url[i] != 0; ++i)
-      {
-        request.path += url[i];
-      }
-      std::string headers;
-      out << "req headers> ";
-      std::getline(in, headers);
-      for (size_t i = 0; i < headers.size(); ++i)
-      {
-        std::string header, value;
-        for (; headers[i] != '='; ++i)
-        {
-          header+=headers[i];
-        }
-        ++i;
-        for (; headers[i] != ' ' &&  headers[i] != '\0'; ++i)
-        {
-          value+=headers[i];
-        }
-        request.headers[header] = value;
-      }
-      out << "req body> ";
-      std::getline(in, request.body);
 
-      std::unique_ptr< cli::Menu > reqMenu = reqInit(name, request);
-      cli::Cli req(std::move(reqMenu));
-      cli::LoopScheduler scheduler;
-      req.ExitAction([&scheduler](std::ostream& out)
+      void setMethod(std::ostream& out, models::Request& request, const std::string& method)
       {
-        scheduler.Stop();
-      });
-      cli::CliLocalTerminalSession session(req, scheduler, std::cout);
-      scheduler.Run();
-    }
-
-    void show(std::ostream& out, const std::string& name, const models::Request& request)
-    {
-      out << "name: " << name << "\n";
-      out << "method: " << request.method << "\n";
-      out << "host: " << request.host << "\n";
-      out << "path: " << request.path << "\n";
-      out << "headers:\n";
-      for (auto it = request.headers.begin(); it != request.headers.end(); ++it)
-      {
-        out << "  " << it->first << ": " << it->second << "\n";
+        request.method = method;
       }
-      out << "body: " << request.body << "\n";
+
+      void setURL(std::ostream& out, models::Request& request, const std::string& url)
+      {
+        size_t i = url.find("://");
+        if (i == std::string::npos)
+        {
+          i = 0;
+        }
+        else {
+          i += 3;
+        }
+        for (; url[i] != '/'; ++i)
+        {
+          request.host += url[i];
+        }
+        for (; url[i] != 0; ++i)
+        {
+          request.path += url[i];
+        }
+      }
+
+      void setHeaders(std::ostream& out, models::Request& request, const std::string& headers)
+      {
+        for (size_t i = 0; i < headers.size(); ++i)
+        {
+          std::string header, value;
+          for (; headers[i] != '='; ++i)
+          {
+            header+=headers[i];
+          }
+          ++i;
+          for (; headers[i] != ' ' &&  headers[i] != '\0'; ++i)
+          {
+            value+=headers[i];
+          }
+          request.headers[header] = value;
+        }
+      }
+
+      void setBody(std::ostream& out, models::Request& request, const std::string& body)
+      {
+        request.body = json::parse(body);
+      }
+
+      void startReqMenu(std::string& name, models::Request& request)
+      {
+        std::unique_ptr< cli::Menu > reqMenu = reqInit(name, request);
+        cli::Cli req(std::move(reqMenu));
+        cli::LoopScheduler scheduler;
+        req.ExitAction([&scheduler](std::ostream& out)
+        {
+          scheduler.Stop();
+        });
+        cli::CliLocalTerminalSession session(req, scheduler, std::cout);
+        scheduler.Run();
+      }
+
+      void req(std::ostream& out)
+      {
+        std::istream& in = std::cin;
+        models::Request request;
+        std::string name;
+        out << "req name> ";
+        std::getline(in, name);
+        std::string method;
+        out << "req method> ";
+        std::getline(in, method);
+        setMethod(out, request, method);
+        std::string url;
+        out << "req URL> ";
+        std::getline(in, url);
+        setURL(out, request, url);
+        std::string headers;
+        out << "req headers> ";
+        std::getline(in, headers);
+        setHeaders(out, request, headers);
+        out << "req body> ";
+        std::string body;
+        std::getline(in, body);
+        setBody(out, request, body);
+
+        startReqMenu(name, request);
+      }
+
+      void show(std::ostream& out, const std::string& name, const models::Request& request)
+      {
+        out << "name: " << name << "\n";
+        out << "method: " << request.method << "\n";
+        out << "host: " << request.host << "\n";
+        out << "path: " << request.path << "\n";
+        out << "headers:\n";
+        for (auto it = request.headers.begin(); it != request.headers.end(); ++it)
+        {
+          out << "  " << it->first << ": " << it->second << "\n";
+        }
+        out << "body: " << request.body << "\n";
+      }
     }
   }
 }
