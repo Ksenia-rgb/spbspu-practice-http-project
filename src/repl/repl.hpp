52c93c57fff94c4e39b84372e0405eabb4768e5b
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-04-14 15:23:01 +0300

    remove test functions

--- src/repl/repl.hpp
+++ src/repl/repl.hpp
@@ -10,22 +10,10 @@ namespace http
 {
   namespace repl
   {
-    inline void sum(std::ostream& out, int a, int b)
-    {
-      out << (a + b) << '\n';
-    }
-
-    inline void sub(std::ostream& out, int a, int b)
-    {
-      out << (a - b) << '\n';
-    }
-
     inline std::unique_ptr< cli::Menu > init()
     {
       auto rootMenu = std::make_unique< cli::Menu >("http-cli");
-      rootMenu->Insert("sum", static_cast<std::function<void(std::ostream&, int, int)>>(sum));
-      rootMenu->Insert("sub", static_cast<std::function<void(std::ostream&, int, int)>>(sub));
-      rootMenu->Insert("req", static_cast<std::function<void(std::ostream&)>>(req));
+      rootMenu->Insert("req", static_cast<std::function<void(std::ostream&)>>(req::req));
       return rootMenu;
     }
   }
