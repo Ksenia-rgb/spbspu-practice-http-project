a97c002e300da5b3f7630520a32fd99320d5c345
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-04-28 00:07:26 +0300

    CG

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,11 @@ int main()
   std::unique_ptr< cli::Menu > rootMenu = http::repl::init();
   cli::Cli app(std::move(rootMenu));
   cli::LoopScheduler scheduler;
-  app.ExitAction([&scheduler](std::ostream&) {
-    scheduler.Stop();
-  });
+  app.ExitAction(
+    [&scheduler](std::ostream&)
+    {
+      scheduler.Stop();
+    });
 
   cli::CliLocalTerminalSession session(app, scheduler, std::cout);
 
