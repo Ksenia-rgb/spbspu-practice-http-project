a30b69384b0925b18fff70678443dd7f2b5362a5
Ksenia_rgb <130300736+Ksenia-rgb@users.noreply.github.com>
2026-05-02 20:48:34 +0300

    Revert "Send impl"

--- src/http/send.hpp
+++ src/http/send.hpp
@@ -1,13 +1,11 @@
-#ifndef SEND_HPP
-#define SEND_HPP
+#include <istream>
 #include <models.hpp>
 
 namespace http
 {
   namespace send
   {
-    models::Response sendRequest(const models::Request& req);
+    http::models::Response sendRequest(const http::models::Request&);
+    http::models::Response sendRequest(std::istream& in);
   }
 }
-
-#endif
